376a6fcaea88de589bd03062d37a13fc854bc5b7
ichlml <ichlamal8899@gmail.com>
2024-12-07 10:18:06 +0700

    initial porto v2

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 px-4 lg:px-0">
+      <div className="max-w-7xl mx-auto flex h-14 items-center">
+        <div className="md:mr-4 flex justify-between w-full">
+          <a href="" className="mr-6 flex items-center space-x-2">
+            <img src="" alt="" />
+            aa
+          </a>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
